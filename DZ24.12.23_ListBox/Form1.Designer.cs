c4eb9f3411b86b9e9c8824407b70a7ebc6ee7e7e
Lyashenko Sergey <2268746@mail.ru>
2023-12-21 18:28:26 +0300

    end

---
+++ DZ24.12.23_ListBox/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace DZ24._12._23_ListBox
+{
+    partial class Anketa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_Name = new System.Windows.Forms.TextBox();
+            this.textBox_LastName = new System.Windows.Forms.TextBox();
+            this.textBox_email = new System.Windows.Forms.TextBox();
+            this.textBox_Phone = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button_Registration = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.button_LoadInfo = new System.Windows.Forms.Button();
+            this.button_Edit = new System.Windows.Forms.Button();
+            this.button_Delete = new System.Windows.Forms.Button();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.button_SaveInfoTXT = new System.Windows.Forms.Button();
+            this.button_SaveInfoXML = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_Name
+            // 
+            this.textBox_Name.Location = new System.Drawing.Point(442, 52);
+            this.textBox_Name.Name = "textBox_Name";
+            this.textBox_Name.Size = new System.Drawing.Size(323, 26);
+            this.textBox_Name.TabIndex = 0;
+            // 
+            // textBox_LastName
+            // 
+            this.textBox_LastName.Location = new System.Drawing.Point(442, 120);
+            this.textBox_LastName.Name = "textBox_LastName";
+            this.textBox_LastName.Size = new System.Drawing.Size(323, 26);
+            this.textBox_LastName.TabIndex = 1;
+            // 
+            // textBox_email
+            // 
+            this.textBox_email.Location = new System.Drawing.Point(442, 194);
+            this.textBox_email.Name = "textBox_email";
+            this.textBox_email.Size = new System.Drawing.Size(323, 26);
+            this.textBox_email.TabIndex = 2;
+            // 
+            // textBox_Phone
+            // 
+            this.textBox_Phone.Location = new System.Drawing.Point(442, 267);
+            this.textBox_Phone.Name = "textBox_Phone";
+            this.textBox_Phone.Size = new System.Drawing.Size(323, 26);
+            this.textBox_Phone.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(153, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Введите ваше имя";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(153, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(197, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Введите вашу фамилию";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(153, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Введите ваш email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(153, 273);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(185, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Введите ваш телефон";
+            // 
+            // button_Registration
+            // 
+            this.button_Registration.Location = new System.Drawing.Point(272, 347);
+            this.button_Registration.Name = "button_Registration";
+            this.button_Registration.Size = new System.Drawing.Size(292, 50);
+            this.button_Registration.TabIndex = 8;
+            this.button_Registration.Text = "Сохранить данные";
+            this.button_Registration.UseVisualStyleBackColor = true;
+            this.button_Registration.Click += new System.EventHandler(this.button_Registration_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 20;
+            this.listBox.Location = new System.Drawing.Point(58, 431);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(706, 224);
+            this.listBox.TabIndex = 12;
+            this.listBox.SelectedIndexChanged += new System.EventHandler(this.listBox_SelectedIndexChanged);
+            // 
+            // button_LoadInfo
+            // 
+            this.button_LoadInfo.Location = new System.Drawing.Point(58, 702);
+            this.button_LoadInfo.Name = "button_LoadInfo";
+            this.button_LoadInfo.Size = new System.Drawing.Size(207, 88);
+            this.button_LoadInfo.TabIndex = 13;
+            this.button_LoadInfo.Text = "Загрузить данные из базы";
+            this.button_LoadInfo.UseVisualStyleBackColor = true;
+            this.button_LoadInfo.Click += new System.EventHandler(this.button_LoadInfo_Click);
+            // 
+            // button_Edit
+            // 
+            this.button_Edit.Location = new System.Drawing.Point(557, 702);
+            this.button_Edit.Name = "button_Edit";
+            this.button_Edit.Size = new System.Drawing.Size(207, 88);
+            this.button_Edit.TabIndex = 14;
+            this.button_Edit.Text = "Редактировать данные";
+            this.button_Edit.UseVisualStyleBackColor = true;
+            this.button_Edit.Click += new System.EventHandler(this.button_Edit_Click);
+            // 
+            // button_Delete
+            // 
+            this.button_Delete.Location = new System.Drawing.Point(557, 828);
+            this.button_Delete.Name = "button_Delete";
+            this.button_Delete.Size = new System.Drawing.Size(207, 88);
+            this.button_Delete.TabIndex = 15;
+            this.button_Delete.Text = "Удалить данные";
+            this.button_Delete.UseVisualStyleBackColor = true;
+            this.button_Delete.Click += new System.EventHandler(this.button_Delete_Click);
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(738, 431);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(26, 224);
+            this.vScrollBar1.TabIndex = 16;
+            // 
+            // button_SaveInfoTXT
+            // 
+            this.button_SaveInfoTXT.Location = new System.Drawing.Point(310, 702);
+            this.button_SaveInfoTXT.Name = "button_SaveInfoTXT";
+            this.button_SaveInfoTXT.Size = new System.Drawing.Size(207, 88);
+            this.button_SaveInfoTXT.TabIndex = 17;
+            this.button_SaveInfoTXT.Text = "Сохранить данные в базу в TXT";
+            this.button_SaveInfoTXT.UseVisualStyleBackColor = true;
+            this.button_SaveInfoTXT.Click += new System.EventHandler(this.button_SaveInfoTXT_Click);
+            // 
+            // button_SaveInfoXML
+            // 
+            this.button_SaveInfoXML.Location = new System.Drawing.Point(310, 828);
+            this.button_SaveInfoXML.Name = "button_SaveInfoXML";
+            this.button_SaveInfoXML.Size = new System.Drawing.Size(207, 88);
+            this.button_SaveInfoXML.TabIndex = 18;
+            this.button_SaveInfoXML.Text = "Сохранить данные в базу в XML";
+            this.button_SaveInfoXML.UseVisualStyleBackColor = true;
+            this.button_SaveInfoXML.Click += new System.EventHandler(this.button_SaveInfoXML_Click);
+            // 
+            // Anketa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 950);
+            this.Controls.Add(this.button_SaveInfoXML);
+            this.Controls.Add(this.button_SaveInfoTXT);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.button_Delete);
+            this.Controls.Add(this.button_Edit);
+            this.Controls.Add(this.button_LoadInfo);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.button_Registration);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Phone);
+            this.Controls.Add(this.textBox_email);
+            this.Controls.Add(this.textBox_LastName);
+            this.Controls.Add(this.textBox_Name);
+            this.Name = "Anketa";
+            this.Text = "Анкета";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_Name;
+        private System.Windows.Forms.TextBox textBox_LastName;
+        private System.Windows.Forms.TextBox textBox_email;
+        private System.Windows.Forms.TextBox textBox_Phone;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button_Registration;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Button button_LoadInfo;
+        private System.Windows.Forms.Button button_Edit;
+        private System.Windows.Forms.Button button_Delete;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.Button button_SaveInfoTXT;
+        private System.Windows.Forms.Button button_SaveInfoXML;
+    }
+}
+
---
+++ DZ24.12.23_ListBox/Form1.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+
+
+namespace DZ24._12._23_ListBox
+{
+    public partial class Anketa : Form
+    {
+        int counter = 0;
+        Dictionary <int,User> usersList=new Dictionary<int, User>();
+        List<string> list = new List<string>();
+        public Anketa()
+        {
+            InitializeComponent();
+        }
+
+        void WriteToTXT(Dictionary<int,User> userList)
+        {
+            using (StreamWriter file = new StreamWriter("base.txt"))
+            {
+                foreach (KeyValuePair<int, User> user in userList)
+                {
+                    file.WriteLine(user.Key + "," + user.Value.Name + "," + user.Value.LastName + "," + user.Value.Email + "," + user.Value.Phone);
+                }
+            }
+        }
+
+        void WriteToXml(Dictionary<int, User> userList)
+        {
+            var serializer = new XmlSerializer(typeof(Dictionary<int, User>));
+            var settings = new XmlWriterSettings
+            {
+                Indent = true,
+                IndentChars = "\t"
+            };
+            using (var writer = XmlWriter.Create("base.xml", settings))
+            {
+                serializer.Serialize(writer, userList);
+            }
+        }
+
+        void ReadFromXml()
+        {
+            usersList.Clear();
+            if (File.Exists("base.xml"))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(Dictionary<int, User>));
+                using (TextReader reader = new StreamReader("base.xml"))
+                {
+                    try
+                    {
+                        usersList = (Dictionary<int, User>)serializer.Deserialize(reader);
+                    }
+                    catch (InvalidOperationException ex)
+                    {
+                        MessageBox.Show("Ошибка при попытке прочитать XML: " + ex.Message);
+                    }
+                }
+                UpdateListBox(usersList);
+            }
+            else
+            {
+                MessageBox.Show("Файл base.xml не найден");
+            }
+        }
+
+        void ReadFromTXT()
+        {
+            usersList.Clear();
+            if (File.Exists("base.txt"))
+            {
+                try
+                {
+                    string[] str = File.ReadAllLines("base.txt");
+                    foreach (string e in str)
+                    {
+                        string[] data = e.Split(',');
+                        if (data.Length >= 5)
+                        {
+                            int userId = int.Parse(data[0]);
+                            string userName = data[1];
+                            string userLastName = data[2];
+                            string userEmail = data[3];
+                            string userPhone = data[4];
+                            User user=new User(userName, userLastName, userEmail, userPhone);
+                            usersList.Add(userId, user);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Данные записаны неверно в base.txt");
+                        }
+                    }
+                    UpdateListBox(usersList);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка чтения base.txt: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Файл base.txt не найден");
+            }
+        }
+
+
+        bool chechedEmpty(TextBox textBox)
+        {
+            if(String.IsNullOrEmpty(textBox.Text))
+            {
+                textBox.BackColor= Color.Red;
+                return false;
+            }
+            else
+            {
+                textBox.BackColor= Color.White;
+                return true;
+            }
+        }
+
+        void UpdateListBox(Dictionary<int, User> userList)
+        {
+            listBox.Items.Clear();
+            foreach (KeyValuePair<int, User> entry in userList)
+            {
+                listBox.Items.Add(entry.Key + ":" + entry.Value.Name + ":" + entry.Value.LastName + ":" + entry.Value.Email + ":" + entry.Value.Phone);
+            }
+        }
+        private void button_Registration_Click(object sender, EventArgs e)
+        {
+            if (chechedEmpty(textBox_Name) && chechedEmpty(textBox_LastName)
+                && chechedEmpty(textBox_email) && chechedEmpty(textBox_Phone))
+            {
+                User user = new User(
+                    textBox_Name.Text,
+                    textBox_LastName.Text,
+                    textBox_email.Text,
+                    textBox_Phone.Text);
+                usersList.Add(++counter,user);
+                UpdateListBox(usersList);
+                textBox_Name.Clear();
+                textBox_LastName.Clear();
+                textBox_email.Clear();
+                textBox_Phone.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Заполните все поля");
+            }
+
+        }
+
+        private void listBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string str=listBox.SelectedItem.ToString();
+            List<string> list1 = str.Split(':').ToList();
+            textBox_Name.Text = list1[1].ToString();
+            textBox_LastName.Text = list1[2].ToString();
+            textBox_email.Text = list1[3].ToString();
+            textBox_Phone.Text = list1[4].ToString();
+            list=list1;
+        }
+
+        private void button_Edit_Click(object sender, EventArgs e)
+        {
+
+            int index = int.Parse(list[0]);
+            if (usersList.ContainsKey(index))
+            {
+                User user = new User(
+                        textBox_Name.Text,
+                        textBox_LastName.Text,
+                        textBox_email.Text,
+                        textBox_Phone.Text);
+                usersList[index] = user;
+                list.Clear();
+                textBox_Name.Clear();
+                textBox_LastName.Clear();
+                textBox_email.Clear();
+                textBox_Phone.Clear();
+                UpdateListBox(usersList);
+            }
+            else
+            {
+                MessageBox.Show("Что-то пошло не так");
+            }
+
+        }
+
+        private void button_Delete_Click(object sender, EventArgs e)
+        {
+            int index = int.Parse(list[0]);
+            if (usersList.ContainsKey(index))
+            {
+                usersList.Remove(index);
+                list.Clear();
+                UpdateListBox(usersList);
+            }
+            else
+            {
+                MessageBox.Show("Что-то пошло не так");
+            }
+            textBox_Name.Clear();
+            textBox_LastName.Clear();
+            textBox_email.Clear();
+            textBox_Phone.Clear();
+        }
+
+        private void button_SaveInfoTXT_Click(object sender, EventArgs e)
+        {
+            if (usersList.Keys.Count > 0)
+            {
+                WriteToTXT(usersList);
+            }
+            else
+            {
+                MessageBox.Show("Список пуст");
+            }
+        }
+
+        private void button_SaveInfoXML_Click(object sender, EventArgs e)
+        {
+            if (usersList.Keys.Count > 0)
+            {
+                WriteToXml(usersList);
+            }
+            else
+            {
+                MessageBox.Show("Список пуст");
+            }
+        }
+
+        private void button_LoadInfo_Click(object sender, EventArgs e)
+        {
+            string filePathTXT = "base.txt";
+            string filePathXML = "base.xml";
+            FileInfo fileInfoTXT= new FileInfo(filePathTXT);
+            DateTime dateTimeTXT = fileInfoTXT.CreationTime;
+            FileInfo fileInfoXML= new FileInfo(filePathXML);
+            DateTime dateTimeXML = fileInfoXML.CreationTime;
+            bool txtExist = false;
+            bool xmlExist = false;
+            int lastDate = -1;
+            
+            if(fileInfoTXT.Exists)
+            {
+                txtExist= true;
+            }
+            if (fileInfoXML.Exists)
+            { 
+                xmlExist = true;
+            }
+            if (txtExist && xmlExist)
+            {
+                if (dateTimeTXT > dateTimeXML)
+                {
+                    lastDate = 0;
+                }
+                else
+                {
+                    lastDate = 1;
+                }
+                if (lastDate == 0)
+                {
+                    ReadFromTXT();
+                }
+                else if (lastDate == 1)
+                {
+                    ReadFromXml();
+                }
+            }
+            else if (txtExist && !xmlExist)
+            {
+                ReadFromTXT();
+            }
+            else if (!txtExist && xmlExist)
+            {
+                ReadFromXml();
+            }
+            else
+            {
+                MessageBox.Show("Файл базы данных не найден");
+            }
+
+
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        public User(string name, string lastName, string email, string phone)
+        {
+            Name = name;
+            LastName = lastName;
+            Email = email;
+            Phone = phone;
+
+        }
+    }
+}
